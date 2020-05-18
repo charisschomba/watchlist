4d2e297316248b1a2375a5d623c670440b03fb88
chariss <charisschomba@gmail.com>
2020-05-18 14:54:12 +0300

    improve responsiveness

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  api_key: 'fc5ed10c0c0bf12eb8aa726da7ce03f6',
+  url: 'https://api.themoviedb.org',
+  imageUrl: 'http://image.tmdb.org/t/p/w500/'
 };
