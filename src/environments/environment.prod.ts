6cc22f03fc64933729556ecc3fad49af3a3ee30e
charisschomba <charisschomba@gmail.com>
2020-05-18 19:55:18 +0300

    host dist

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  api_key: 'fc5ed10c0c0bf12eb8aa726da7ce03f6',
-  url: 'https://api.themoviedb.org',
-  imageUrl: 'http://image.tmdb.org/t/p/w500/'
-};
